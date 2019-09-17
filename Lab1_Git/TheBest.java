35a748bcb902e0b212319bd0fdf82e90dcc31f5e
thatspartan <dumasdo@outlook.com>
2019-09-16 20:13:54 -0400

    added 2 person to output

--- Lab1_Git/TheBest.java
+++ Lab1_Git/TheBest.java
@@ -3,6 +3,7 @@ public class TheBest {
     
     public static void main(String[] args) {
 	PersonInfo1.displayEn();
-	
+	PersonInfo2.displayEn();
+	PersonInfo3.displayEn();	
     }
 }
---
+++ Lab1_Git/~.bash_profile
@@ -0,0 +1,1 @@
+alias j8="export JAVA_HOME=`/usr/libexec/java_home -v 1.8`; java -version"
